0c6ccb0a32352d7fd6b4c6ba541febf10afce2b5
Jeremy Lindsay <jeremy@jeremylindsay.com>
2025-04-04 00:16:32 +1100

    solution

--- MIL/C02_Basics/S01_Calculating.lean
+++ MIL/C02_Basics/S01_Calculating.lean
@@ -45,7 +45,9 @@ example (a b c d e f : ℝ) (h : b * c = e * f) : a * b * c * d = a * e * f * d 
   rw [← mul_assoc]
 
 example (a b c d : ℝ) (hyp : c = b * a - d) (hyp' : d = a * b) : c = 0 := by
-  sorry
+  rw [hyp]
+  rw [hyp']
+  ring
 
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h', ← mul_assoc, h, mul_assoc]
