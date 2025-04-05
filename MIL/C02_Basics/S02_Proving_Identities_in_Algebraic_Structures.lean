baa1f2f8317322254b2b310f73fb9a7e939be0d9
Jeremy Lindsay <jeremy@jeremylindsay.com>
2025-04-05 11:35:29 +1100

    solution (2.2)

--- MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
+++ MIL/C02_Basics/S02_Proving_Identities_in_Algebraic_Structures.lean
@@ -76,7 +76,7 @@ theorem zero_mul (a : R) : 0 * a = 0 := by
   rw [add_left_cancel h]
 
 theorem neg_eq_of_add_eq_zero {a b : R} (h : a + b = 0) : -a = b := by
-  sorry
+  rw [← neg_add_cancel_left a b, h, add_zero]
 
 theorem eq_neg_of_add_eq_zero {a b : R} (h : a + b = 0) : a = -b := by
   sorry
