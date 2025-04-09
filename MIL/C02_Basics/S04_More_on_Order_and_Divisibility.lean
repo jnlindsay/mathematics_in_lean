77c86fc217c094dca8f2236de3baf8fef7f1b1f5
Jeremy Lindsay <jeremy@jeremylindsay.com>
2025-04-09 21:06:34 +1000

    solution (2.4)

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,7 +39,15 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  have h : ∀ x y : ℝ, max x y ≤ max y x := by
+    intro x y
+    apply max_le
+    . apply le_max_right
+    . apply le_max_left
+  apply le_antisymm
+  . apply h
+  . apply h
+
 example : min (min a b) c = min a (min b c) := by
   sorry
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
