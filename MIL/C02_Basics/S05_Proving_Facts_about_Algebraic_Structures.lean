bd1cfb370199d80a053544f00369b98b90fcd9a4
Jeremy Lindsay <jeremy@jeremylindsay.com>
2025-04-12 09:30:51 +1000

    solution (2.5)

--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -36,7 +36,11 @@ variable (x y z : α)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
 example : x ⊓ y = y ⊓ x := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply le_inf
+    . apply inf_le_right
+    . apply inf_le_left
 
 example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
   sorry
@@ -109,4 +113,3 @@ example (x y : X) : 0 ≤ dist x y := by
   sorry
 
 end
-
